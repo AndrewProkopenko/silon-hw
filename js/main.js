287dc22f3973c6cdd93db292fb73f06ddc46a3b5
AndrewProkopenko <i.andrewprokopenko@gmail.com>
2019-11-21 14:51:28 +0300

    21.11 morning commit

--- js/main.js
+++ js/main.js
@@ -8,10 +8,34 @@ $(document).ready(function(){
         responsive: { 
             0: { 
                 items: 1,
-                margin: 0,
+                margin: 15,
             },
             900: { 
                 items: 3,
+                margin: 30,
+            }
+            
+        }
+    });
+  });
+$(document).ready(function(){
+    $("#popular-carousel").owlCarousel({        
+        // autoplay: true, 
+        autoplayTimeout: 4000,
+        autoplaySpeed: 2000,
+        autoplayHoverPause: true,
+        loop: true,
+        responsive: { 
+            0: { 
+                items: 1,
+                margin: 15,
+            },
+            700: { 
+                items: 3,
+                margin: 15,
+            },
+            1100: { 
+                items: 4,
                 margin: 30,
             }
             
